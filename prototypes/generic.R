d556a8b38c71b58ebdd83a23b73117e0bfc2194d
rmvegasm <rmvegasm@gmail.com>
2019-03-14 21:57:32 -0300

    removed unused files

--- prototypes/generic.R
+++
@@ -1,7 +0,0 @@
-setMethod("show",
-    signature(object = "class"),
-    function (object) 
-    {
-        stop("need a definition for the method here")
-    }
-)
--- prototypes/initialize.R
+++
@@ -1,7 +0,0 @@
-setMethod("initialize",
-    signature(.Object = "class"),
-    function (.Object, ...) 
-    {
-        stop("need a definition for the method here")
-    }
-)
