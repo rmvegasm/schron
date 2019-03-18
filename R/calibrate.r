cf6569b6c1e255bc9ba38203ab371c0e5c405243
rmvegasm <rmvegasm@gmail.com>
2019-03-18 14:35:30 -0300

    included cal curve in the package

--- R/calibrate.r
+++ R/calibrate.r
@@ -1,3 +1,17 @@
+#' @title Calibrate 14C age determinations
+#'
+#' @param age a numeric vector containing the mean 14C age for the samples.
+#'
+#' @param error a numeric vector containing the error for each 14C
+#' determination.
+#'
+#' @param curve a string, currently only `"SHcal13.txt"` implemented.
+#'
+#' @param eps the smallest probability to be considered as something. Values
+#' under this number will not be included in the `pmf` output.
+#'
+#' @return a list of `pmf` objects.
+#' @export
 calibrate <- function (age, error, curve = 'SHcal13.txt', eps = 1e-5) {
   curve <- read_curve(curve)
   cal_one <- function (age, error) {
@@ -8,13 +22,13 @@ calibrate <- function (age, error, curve = 'SHcal13.txt', eps = 1e-5) {
   }
   if (length(age) != length(error)) stop('age and error lengths differ')
   n <- length(age)
-  if (n == 1L) return(cal_one(age, error))
   cal <- list()
   for (i in 1:n) cal[[i]] <- cal_one(age[i], error[i])
   cal
 }
 read_curve <- function(file) {
-  curve <- read.table(file, sep = ',', col.names = c('cal', 'rc_age', 'rc_err',
+  path <- system.file('extdata', file, package = 'schron', mustWork = TRUE)
+  curve <- read.table(path, sep = ',', col.names = c('cal', 'rc_age', 'rc_err',
     'delta', 'sigma'))[, 1:3]
   year <- 50000L:0L
   toyear <- function (x) with(curve, approx(cal, x, xout = year)$y)
