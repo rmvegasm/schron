27cb3bff44a52caf12fc5411b91e023f8edac8c0
rmvegasm <rmvegasm@gmail.com>
2019-11-04 18:25:50 -0300

    added error messages, improved class requirements

--- R/class-core.r
+++ R/class-core.r
@@ -21,21 +21,32 @@ valid.core <- function (x) {
     ),
     is.character(x$name),
     is.numeric(x$extent),
-    is.integer(x$year)
+    is.numeric(x$year)
   )
   len <- c(
     length(x$name)   == 1L,
     length(x$extent) == 1L,
     length(x$year)   == 1L
   )
-  msg <- c(
-    '`dates` should be a tibble, or coersible to one',
-    '`events` should be a tibble, or coersible to one',
-    '`name` should be a character',
-    '`extent` should be numeric',
-    '`year` should be an integer'
+  tbl <- c(
+    all(c('age', 'error', 'depth') %in% names(x$dates)),
+    all(c('base_depth', 'top_depth', 'event_id') %in% names(x$events)),
+    with(x$events, all(base_depth - top_depth > 0L))
   )
-  cond <- c(cls, len)
+  msg <- c(
+    '`dates` must be a tibble, or coersible to one',
+    '`events` must be a tibble, or coersible to one',
+    '`name` must be a character',
+    '`extent` must be numeric',
+    '`year` must be numeric',
+    '`name` must be of length 1',
+    '`extent` must be of length 1',
+    '`year` must be of length 1',
+    '`dates` must have at least `age`, `error` and `depth` columns',
+    '`events` must have at least `base_depth`, `top_depth` and `event_id` columns',
+    'in `events`: found inconsistent base and top depths for some event layers'
+  )
+  cond <- c(cls, len, tbl)
   if (all(cond)) return(TRUE)
   stop(paste('\n', c('invalid core:', msg[!cond])))
 }
